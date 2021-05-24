82293560375c4d52c91c8b78b4bc92f4c0b270a9
Michael Bock <me@thought2.de>
2021-05-24 12:08:39 +0200

    Use chainFirst

--- src/FileSystem.ts
+++ src/FileSystem.ts
@@ -4,7 +4,7 @@ import * as TE from 'fp-ts/lib/TaskEither'
 import * as R from 'fp-ts/Record'
 import * as path from 'path'
 
-import { pipe } from 'fp-ts/lib/function'
+import { constVoid, pipe } from 'fp-ts/lib/function'
 import { Config } from './Config'
 import { FileObj, print } from './FileObj'
 
@@ -16,8 +16,8 @@ export const writeOut = ({ config }: { config: Config }) => (
   pipe(
     RTE.ask<Capabilities>(),
     RTE.map((cap) => ({ cap })),
-    RTE.bind('_1', ({ cap }) => cap.mkDir(config.name, { recursive: false })),
-    RTE.bind('_2', ({ cap }) =>
+    RTE.chainFirst(({ cap }) => cap.mkDir(config.name, { recursive: false })),
+    RTE.chainFirst(({ cap }) =>
       pipe(
         fileSystem,
         R.traverseWithIndex(RTE.ApplicativeSeq)((k, v) =>
@@ -25,11 +25,11 @@ export const writeOut = ({ config }: { config: Config }) => (
             RTE.Do,
             RTE.bind('filePath', () => RTE.of(path.join(config.name, k))),
             RTE.bind('dirPath', (s) => RTE.of(path.dirname(s.filePath))),
-            RTE.bind('_1', (s) => cap.mkDir(s.dirPath, { recursive: true })),
-            RTE.bind('_2', (s) => cap.writeFile(s.filePath, print(v)))
+            RTE.chainFirst((s) => cap.mkDir(s.dirPath, { recursive: true })),
+            RTE.chainFirst((s) => cap.writeFile(s.filePath, print(v)))
           )
         )
       )
     ),
-    RTE.map((x) => undefined as void)
+    RTE.map(constVoid)
   )
