608e6fdeb567b6bbd6c521cac8190d32c112c379
Anthony Gabriele <anthonyjoeseph@gmail.com>
2022-04-08 02:47:36 -0400

    use package.json exports field

--- src/features/skeleton/package-json.ts
+++ src/features/skeleton/package-json.ts
@@ -5,7 +5,13 @@ export default (config: Config): PackageJson => ({
   name: config.name,
   homepage: config.homepage,
   version: config.projectVersion,
-  main: 'dist/index.js',
+  main: 'dist/lib/index.js',
+  exports: {
+    "*": {
+      require: "./dist/lib/index.js",
+      import: "./dist/es6/index.js"
+    }
+  },
   license: config.license,
   peerDependencies: {
     'fp-ts': '^2.9.5',
@@ -16,7 +22,7 @@ export default (config: Config): PackageJson => ({
     'fp-ts': '^2.9.5',
   },
   scripts: {
-    build: 'tsc -p tsconfig.build.json',
+    build: 'tsc -p tsconfig.build.json && tsc -p tsconfig.build-es6.json',
     'build:watch': 'tsc -w -p tsconfig.build.json',
     prepublish: `${config.packageManager} run build`,
   },
